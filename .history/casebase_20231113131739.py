44b246eab65728293c56fcbdc62fea0ed269f06c
niltonma <niltonmonteiroalves@aluno.puc-rio.br>
2023-11-13 13:27:16 -0300

    ALTERACAO TERMINAL MEDICAO

--- .history/casebase_20231113131739.py
+++ .history/casebase_20231113131739.py
@@ -21,18 +21,18 @@ dss.text("set stepsize=0.25h")
 dss.text("New EnergyMeter.medidor1 element=Transformer.XFM1 terminal=1")
 dss.text("New EnergyMeter.medidor2 element=Line.632633  terminal=2")
 
-dss.text("New monitor.powers1 action=Save element=Transformer.XFM1  terminal=1 ppolar=no mode=1") # mode= 1 medir Potencia ativa
+dss.text("New monitor.powers1 action=Save element=Transformer.XFM1  terminal=2 ppolar=no mode=1") # mode= 1 medir Potencia ativa
 dss.text("New monitor.powers2 action=Save element=Line.632633  terminal=1 ppolar=no mode=1")
-dss.text("New monitor.Current1 action=Save element=Transformer.XFM1 terminal=1 ppolar=no mode=0")
+dss.text("New monitor.Current1 action=Save element=Transformer.XFM1 terminal=2 ppolar=no mode=0")
 
 # dss.text("New monitor.powers1 action=Save element=Transformer.XFM1  terminal=2 ppolar=yes mode=0") # mode= 0 medir tensao
 # dss.text("New monitor.powers2 action=Save element=Line.632633  terminal=2 ppolar=yes mode=0")
 
 
 #carga original, caso base - CASE A:
-dss.text("edit Load.634a Bus1=634.1     Phases=1 Conn=Wye  Model=1 kV=0.277  kW=160   kvar=110 daily=DEFAULT")
-dss.text("edit Load.634b Bus1=634.2     Phases=1 Conn=Wye  Model=1 kV=0.277  kW=120   kvar=90  daily=DEFAULT")
-dss.text("edit Load.634c Bus1=634.3     Phases=1 Conn=Wye  Model=1 kV=0.277  kW=120   kvar=90  daily=DEFAULT")
+dss.text("edit Load.634a Bus1=634.1     Phases=1 Conn=Wye  Model=1 kV=0.277  kW=-160   kvar=-110 daily=DEFAULT")
+dss.text("edit Load.634b Bus1=634.2     Phases=1 Conn=Wye  Model=1 kV=0.277  kW=-120   kvar=-90  daily=DEFAULT")
+dss.text("edit Load.634c Bus1=634.3     Phases=1 Conn=Wye  Model=1 kV=0.277  kW=-120   kvar=-90  daily=DEFAULT")
 
 dss.solution.solve()
 
